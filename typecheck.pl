86e5e5fa0e0bf420380053b9b815cc3a56cd49ae
RJ MacReady <82rjmacready@gmail.com>
2013-06-04 16:00:02 -0700

    hmmmm...working for functions :D

--- typecheck.pl
+++ typecheck.pl
@@ -19,6 +19,13 @@ wrap-env(A, env([], A)).
 unwrap-env(env(_, A), A).
 
 
+init-env(A4) :-
+    add-env("print", [function, [integer], [integer]], A1),
+    add-env("print", [function, [float], [integer]], A1, A2),
+    add-env("identity", [function, [integer], [integer]], A2, A3), % 
+    add-env("identity", [function, [float], [float]], A3, A4).
+
+
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Basis
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -27,7 +34,7 @@ unwrap-env(env(_, A), A).
 type-of-w-env(_, _, [int, _], integer).
 type-of-w-env(_, _, [float, _], float).
 % coerce integers to floats, if need be
-type-of-w-env(_, _, [int, _], float).
+% type-of-w-env(_, _, [int, _], float).
 
 type-of-w-env(_, _, [list, []], list(A)).
 type-of-w-env(Env, _, [list, [Head|[]]], list(A)) :- type-of-w-env(Env, Head, A).
@@ -37,29 +44,42 @@ type-of-w-env(Env, _, [list, [Head|Tail]], list(A)) :-
 type-of-w-env(Env, _, [id, Id], A) :-
     search-env(Id, Env, A).
 
-callable([function|_]).
+type-of-callable([function|_]).
+args([function, Args, _], Args).
+ret([function, _, Ret], Ret).
 
-type-of-w-env(Env, _, [application, target | args ], A) :-
+
+
+type-of-w-env(Env, _, [application, Target | Args ], A) :-
     % check type of target (callable?)
-    type-of-w-env(Env, _, target, B),
-    callable(B),
-    % check type of args
-    
-    % A is the third member of B
+    type-of-w-env(Env, _, Target, TypeOfTarget),
+    type-of-callable(TypeOfTarget),
+
+    % check type of args with whats in TypeOfTarget
+    args(TypeOfTarget, TypeOfTargetArgs),
+
+    type-of-seq(Env, _, Args, TypeOfArgs),
+
+    % match(TypeOfArgs, _),
+    maplist(=, TypeOfTargetArgs, TypeOfArgs),
+
+    % A is the third member of TypeOfTarget
+    ret(TypeOfTarget, A).
 
 % Type-check a sequence of expressions
-type-of-seq(Env, _, [OnlyOne|[]], Type) :-
+type-of-seq(Env, _, [OnlyOne], [Type]) :-
     type-of-w-env(Env, _, OnlyOne, Type).
 
-type-of-seq(Env, NewEnv, [First|Rest], Type) :-
-    type-of-w-env(Env, NewEnv, First, _),
+type-of-seq(Env, NewEnv, [First|Rest], [TypeH|Type]) :-
+    type-of-w-env(Env, NewEnv, First, TypeH),
     type-of-seq(NewEnv, _, Rest, Type).
+
 
 % Entry point.
 % Type-check a program
 type-of(Stuff, Type) :-
-    add-env("print", [function, [integer], [integer]], A),
-    type-of-seq(A, _, Stuff, Type).
+    init-env(A4),
+    type-of-seq(A4, _, Stuff, Type).
 
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -73,3 +93,7 @@ test(2, A) :- type-of(_, [list, [[int, 1]]], A).
 test(3, A) :- type-of(_, [int, 1], A).
 test(4, A) :- type-of(_, [float, 1.0], A).
 test(5, A) :- type-of(_, [list, [[int, 1], [float, 2]]], A).
+test(6, A) :- type-of([[application, [id, "print"], [int, 1]]], A).
+test(7, A) :- type-of([[application, [id, "print"], [float, 1]]], A).
+test(8, A) :- type-of([[application, [id, "identity"], [int, 1]]], A).
+test(9, A) :- type-of([[application, [id, "identity"], [float, 1]]], A).
