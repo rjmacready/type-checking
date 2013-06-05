81c7ce70b493f2978733a09607ea78cd721d80ff
RJ MacReady <82rjmacready@gmail.com>
2013-06-05 14:03:16 -0700

    bug solved, some cleanup. now compiles without warnings

--- typecheck.pl
+++ typecheck.pl
@@ -2,25 +2,25 @@
 % One function may have different signatures! do not stop on the first match!
 % We cant use assocs here, dont allow multiple binds ...
 
-search-env(Key, env([[Key, Value]|_], _), Value).
+search_env(Key, env([[Key, Value]|_], _), Value).
 
-search-env(Key, env([_|Rest], Inner), Value) :-
-    search-env(Key, env(Rest, Inner), Value).
+search_env(Key, env([_|Rest], Inner), Value) :-
+    search_env(Key, env(Rest, Inner), Value).
 
-search-env(Key, env([], Inner), Value) :-
-    search-env(Key, Inner, Value).
+search_env(Key, env([], Inner), Value) :-
+    search_env(Key, Inner, Value).
 
 
 % This one appends a new bind to the given env
-add-env(Key, Value, env(Binds, Inner), env([[Key, Value]|Binds], Inner)).
+add_env(Key, Value, env(Binds, Inner), env([[Key, Value]|Binds], Inner)).
 
 % This one creates a new env with a key-value
-add-env(Key, Value, env([[Key, Value]], nil)).
+add_env(Key, Value, env([[Key, Value]], nil)).
 
 
-wrap-env(A, env([], A)).
+wrap_env(A, env([], A)).
 
-unwrap-env(env(_, A), A).
+unwrap_env(env(_, A), A).
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Our types
@@ -44,48 +44,48 @@ type_star([type(_)|Rest]) :-
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-init-env(A5) :-
-%    add-env("print", [function, [integer], [integer]], A1),
-%    add-env("print", [function, [float], [integer]], A1, A2),
-%    add-env("identity", [function, [integer], [integer]], A2, A3), % 
-%    add-env("identity", [function, [float], [float]], A3, A4).
+init_env(A5) :-
+    add_env("print", [function, [type(integer)], [type(integer)]], A1),
+    add_env("print", [function, [type(float)], [type(integer)]], A1, A2),
+%    add_env("identity", [function, [integer], [integer]], A2, A3), % 
+%    add_env("identity", [function, [float], [float]], A3, A4).
 % HOLY SHIT IM SMART
-%    add-env("identity", [function, [A], [A]], A2, A3),
-    add-env("some-fun", [function, [integer], [float]], A4), % 3, A4),
-    add-env("map", [function, [[function, [A], [B]], list(A)], [list(B)]], A4, A5).
+    add_env("identity", [function, [A], [A]], A2, A3),
+    add_env("some-fun", function([type(integer)], [type(float)]), A3, A4),
+    add_env("map", function([function([A], [B]), type(list(A)) ], [type(list(B))]) , A4, A5).
 
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Basis
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+type_of_callable(function(_, _)).
+args(function(Args, _), Args).
+ret(function(_, Ret), Ret).
+
+
 % Type-check of expressions
-type-of-w-env(Env, Env, [int, _], integer).
-type-of-w-env(Env, Env, [float, _], float).
-% coerce integers to floats, if need be
-% type-of-w-env(Env, Env, [int, _], float).
+type_of_w_env(Env, Env, [int, _], type(integer) ).
+type_of_w_env(Env, Env, [float, _], type(float) ).
 
-type-of-w-env(Env, Env, [list, []], list(A)).
-type-of-w-env(Env, Env, [list, [Head]], list(A)) :- type-of-w-env(Env, _, Head, A).
-type-of-w-env(Env, Env, [list, [Head|Tail]], list(A)) :-
-    type-of-w-env(Env, Env, Head, A), type-of-w-env(Env, _, [list, Tail], list(A)).
+type_of_w_env(Env, Env, [list, []], type(list(_)) ).
+type_of_w_env(Env, Env, [list, [Head|Tail]], type(list(A)) ) :-
+    type_of_w_env(Env, Env, Head, A), 
+    type_of_w_env(Env, _, [list, Tail], type(list(A)) ).
 
-type-of-w-env(Env, Env, [id, Id], A) :-
-    search-env(Id, Env, A).
+type_of_w_env(Env, Env, [id, Id], A) :-
+    search_env(Id, Env, A).
 
-type-of-callable([function|_]).
-args([function, Args, _], Args).
-ret([function, _, Ret], Ret).
-
-type-of-w-env(Env, Env, [application, Target | Args ], A) :-
+type_of_w_env(Env, Env, [application, Target | Args ], A) :-
     % check type of target (callable?)
-    type-of-w-env(Env, _, Target, TypeOfTarget),
-    type-of-callable(TypeOfTarget),
+    type_of_w_env(Env, _, Target, TypeOfTarget),
+    type_of_callable(TypeOfTarget),
 
     % check type of args with whats in TypeOfTarget
     args(TypeOfTarget, TypeOfTargetArgs),
 
-    type-of-seq(Env, _, Args, TypeOfArgs),
+    type_of_seq(Env, _, Args, TypeOfArgs),
 
     % match(TypeOfArgs, _),
     maplist(=, TypeOfTargetArgs, TypeOfArgs),
@@ -93,27 +93,27 @@ type-of-w-env(Env, Env, [application, Target | Args ], A) :-
     % A is the third member of TypeOfTarget
     ret(TypeOfTarget, A).
 
-type-of-w-env(Env, Env, [new_var, Var_name, Expr, Cont], T) :-
-    type-of-w-env(Env, _, Expr, TypeExpr),
-    wrap-env(Env, Wenv),
-    add-env(Var_name, TypeExpr, Wenv, NewEnv),
-    type-of-seq(NewEnv, _, Cont, TypeCont),
+type_of_w_env(Env, Env, [new_var, Var_name, Expr, Cont], T) :-
+    type_of_w_env(Env, _, Expr, TypeExpr),
+    wrap_env(Env, Wenv),
+    add_env(Var_name, TypeExpr, Wenv, NewEnv),
+    type_of_seq(NewEnv, _, Cont, TypeCont),
     last(TypeCont, T).
 
 
 
 % Type-check a sequence of expressions
-type-of-seq(_, _, [], []).
-type-of-seq(Env, NewEnv, [First|Rest], [TypeH|Type]) :-
-    type-of-w-env(Env, NewEnv, First, TypeH),
-    type-of-seq(NewEnv, _, Rest, Type).
+type_of_seq(_, _, [], []).
+type_of_seq(Env, NewEnv, [First|Rest], [TypeH|Type]) :-
+    type_of_w_env(Env, NewEnv, First, TypeH),
+    type_of_seq(NewEnv, _, Rest, Type).
 
 
 % Entry point.
 % Type-check a program
-type-of(Stuff, Type) :-
-    init-env(A4),
-    type-of-seq(A4, _, Stuff, Type).
+type_of(Stuff, Type) :-
+    init_env(A4),
+    type_of_seq(A4, _, Stuff, Type).
 
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -121,16 +121,16 @@ type-of(Stuff, Type) :-
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-test( 0, A) :- type-of([[list, [[int, 1], [string, 2]]]], A).
-test( 1, A) :- type-of([[list, [[int, 1], [int, 2]]]], A).
-test( 2, A) :- type-of([[list, [[int, 1]]]], A).
-test( 3, A) :- type-of([[int, 1]], A).
-test( 4, A) :- type-of([[float, 1.0]], A).
-test( 5, A) :- type-of([[list, [[int, 1], [float, 2]]]], A).
-test( 6, A) :- type-of([[application, [id, "print"], [int, 1]]], A).
-test( 7, A) :- type-of([[application, [id, "print"], [float, 1]]], A).
-test( 8, A) :- type-of([[application, [id, "identity"], [int, 1]]], A).
-test( 9, A) :- type-of([[application, [id, "identity"], [float, 1]]], A).
-test(10, A) :- type-of([[new_var, "a", [int, 1], [[id, "a"]]]], A).
-test(11, A) :- type-of([ [application, [id, "map"], [id, "some-fun"], [list, [[int, 1]]]] ], A).
+test( 0, A) :- type_of([[list, [[int, 1], [string, 2]]]], A).
+test( 1, A) :- type_of([[list, [[int, 1], [int, 2]]]], A).
+test( 2, A) :- type_of([[list, [[int, 1]]]], A).
+test( 3, A) :- type_of([[int, 1]], A).
+test( 4, A) :- type_of([[float, 1.0]], A).
+test( 5, A) :- type_of([[list, [[int, 1], [float, 2]]]], A).
+test( 6, A) :- type_of([[application, [id, "print"], [int, 1]]], A).
+test( 7, A) :- type_of([[application, [id, "print"], [float, 1]]], A).
+test( 8, A) :- type_of([[application, [id, "identity"], [int, 1]]], A).
+test( 9, A) :- type_of([[application, [id, "identity"], [float, 1]]], A).
+test(10, A) :- type_of([[new_var, "a", [int, 1], [[id, "a"]]]], A).
+test(11, A) :- type_of([ [application, [id, "map"], [id, "some-fun"], [list, [[int, 1]]]] ], A).
 
