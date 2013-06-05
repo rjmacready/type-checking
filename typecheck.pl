311336dea881be446e619e83fc4b6187a0c2d239
RJ MacReady <82rjmacready@gmail.com>
2013-06-05 14:15:12 -0700

    solved another bug. gotta be carefull with those environments ...

--- typecheck.pl
+++ typecheck.pl
@@ -45,14 +45,12 @@ type_star([type(_)|Rest]) :-
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 init_env(A5) :-
-    add_env("print", [function, [type(integer)], [type(integer)]], A1),
-    add_env("print", [function, [type(float)], [type(integer)]], A1, A2),
-%    add_env("identity", [function, [integer], [integer]], A2, A3), % 
-%    add_env("identity", [function, [float], [float]], A3, A4).
+    add_env(print, function([type(integer)], [type(integer)]), A1),
+    add_env(print, function([type(float)], [type(integer)]), A1, A2),
 % HOLY SHIT IM SMART
-    add_env("identity", [function, [A], [A]], A2, A3),
-    add_env("some-fun", function([type(integer)], [type(float)]), A3, A4),
-    add_env("map", function([function([A], [B]), type(list(A)) ], [type(list(B))]) , A4, A5).
+    add_env(identity, function([A], [A]), A2, A3),
+    add_env(some_fun, function([type(integer)], [type(float)]), A3, A4),
+    add_env(map, function([function([A], [B]), type(list(A)) ], [type(list(B))]) , A4, A5).
 
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -61,29 +59,34 @@ init_env(A5) :-
 
 
 type_of_callable(function(_, _)).
-args(function(Args, _), Args).
-ret(function(_, Ret), Ret).
+fun_args(function(Args, _), Args).
+fun_ret(function(_, Ret), Ret).
 
 
 % Type-check of expressions
+
+% Scalars
 type_of_w_env(Env, Env, [int, _], type(integer) ).
 type_of_w_env(Env, Env, [float, _], type(float) ).
 
+% List
 type_of_w_env(Env, Env, [list, []], type(list(_)) ).
 type_of_w_env(Env, Env, [list, [Head|Tail]], type(list(A)) ) :-
     type_of_w_env(Env, Env, Head, A), 
     type_of_w_env(Env, _, [list, Tail], type(list(A)) ).
 
+% Search the env
 type_of_w_env(Env, Env, [id, Id], A) :-
     search_env(Id, Env, A).
 
+% Function application
 type_of_w_env(Env, Env, [application, Target | Args ], A) :-
     % check type of target (callable?)
     type_of_w_env(Env, _, Target, TypeOfTarget),
     type_of_callable(TypeOfTarget),
 
     % check type of args with whats in TypeOfTarget
-    args(TypeOfTarget, TypeOfTargetArgs),
+    fun_args(TypeOfTarget, TypeOfTargetArgs),
 
     type_of_seq(Env, _, Args, TypeOfArgs),
 
@@ -91,15 +94,15 @@ type_of_w_env(Env, Env, [application, Target | Args ], A) :-
     maplist(=, TypeOfTargetArgs, TypeOfArgs),
 
     % A is the third member of TypeOfTarget
-    ret(TypeOfTarget, A).
+    fun_ret(TypeOfTarget, A).
 
+% Assignment - for lambdas ...
 type_of_w_env(Env, Env, [new_var, Var_name, Expr, Cont], T) :-
     type_of_w_env(Env, _, Expr, TypeExpr),
     wrap_env(Env, Wenv),
     add_env(Var_name, TypeExpr, Wenv, NewEnv),
     type_of_seq(NewEnv, _, Cont, TypeCont),
     last(TypeCont, T).
-
 
 
 % Type-check a sequence of expressions
@@ -127,10 +130,10 @@ test( 2, A) :- type_of([[list, [[int, 1]]]], A).
 test( 3, A) :- type_of([[int, 1]], A).
 test( 4, A) :- type_of([[float, 1.0]], A).
 test( 5, A) :- type_of([[list, [[int, 1], [float, 2]]]], A).
-test( 6, A) :- type_of([[application, [id, "print"], [int, 1]]], A).
-test( 7, A) :- type_of([[application, [id, "print"], [float, 1]]], A).
-test( 8, A) :- type_of([[application, [id, "identity"], [int, 1]]], A).
-test( 9, A) :- type_of([[application, [id, "identity"], [float, 1]]], A).
-test(10, A) :- type_of([[new_var, "a", [int, 1], [[id, "a"]]]], A).
-test(11, A) :- type_of([ [application, [id, "map"], [id, "some-fun"], [list, [[int, 1]]]] ], A).
+test( 6, A) :- type_of([[application, [id, print], [int, 1]]], A).
+test( 7, A) :- type_of([[application, [id, print], [float, 1]]], A).
+test( 8, A) :- type_of([[application, [id, identity], [int, 1]]], A).
+test( 9, A) :- type_of([[application, [id, identity], [float, 1]]], A).
+test(10, A) :- type_of([[new_var, a, [int, 1], [[id, a]]]], A).
+test(11, A) :- type_of([ [application, [id, map], [id, some_fun], [list, [[int, 1]]]] ], A).
 
