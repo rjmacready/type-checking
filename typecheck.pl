8ff0583c83f3af6772ccfaf56d216a721678fadd
RJ MacReady <82rjmacready@gmail.com>
2013-06-05 12:42:51 -0700

    seems ok for map, but repeats results

--- typecheck.pl
+++ typecheck.pl
@@ -1,29 +1,42 @@
 
-search-env(Key, env([[Key, Value]| _], _), Value). % :- write(found).
+search-env(Key, env(Binds, _), Value) :-
+    get_assoc(Key, Binds, Value).
 
-search-env(Key, env([], Inner), Value) :-
+search-env(Key, env(_, Inner), Value) :-
     search-env(Key, Inner, Value).
 
-search-env(Key, env([_|Tail], Inner), Value) :- 
-    search-env(Key, env(Tail, Inner), Value).
+
+% This one appends a new bind to the given env
+add-env(Key, Value, env(Binds, Inner), env(NewBinds, Inner)) :-
+    put_assoc(Key, Binds, Value, NewBinds).
+
+% This one creates a new env with a key-value
+add-env(Key, Value, env(Binds, nil)) :-
+    empty_assoc(Binds0),
+    put_assoc(Key, Binds0, Value, Binds).
 
 
-add-env(Key, Value, env(Binds, Inner), env([[Key, Value]|Binds], Inner)).
-add-env(Key, Value, env([[Key, Value]], [])).
-
-
-wrap-env(A, env([], A)).
+wrap-env(A, env(E, A)) :- empty_assoc(E).
 
 unwrap-env(env(_, A), A).
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-init-env(A4) :-
+% type(integer).
+% type(float).
+% type(list(type(A))) :- type(A).
+% type(function([], [type(A)])) :- type(A).
+
+
+init-env(A5) :-
     add-env("print", [function, [integer], [integer]], A1),
     add-env("print", [function, [float], [integer]], A1, A2),
 %    add-env("identity", [function, [integer], [integer]], A2, A3), % 
 %    add-env("identity", [function, [float], [float]], A3, A4).
 % HOLY SHIT IM SMART
-    add-env("identity", [function, [A], [A]], A2, A4).
+    add-env("identity", [function, [A], [A]], A2, A3),
+    add-env("some-fun", [function, [integer], [float]], A3, A4),
+    add-env("map", [function, [[function, [A], [B]], list(A)], [list(B)]], A4, A5).
 
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -64,6 +77,13 @@ type-of-w-env(Env, Env, [application, Target | Args ], A) :-
     % A is the third member of TypeOfTarget
     ret(TypeOfTarget, A).
 
+type-of-w-env(Env, Env, [new_var, Var_name, Expr, Cont], T) :-
+    type-of-w-env(Env, _, Expr, TypeExpr),
+    wrap-env(Env, Wenv),
+    add-env(Var_name, TypeExpr, Wenv, NewEnv),
+    type-of-seq(NewEnv, _, Cont, TypeCont),
+    last(TypeCont, T).
+
 
 % type-of-w-env(_, _, A, _) :- write("Invalid stuff."), write(A), fail().
 
@@ -89,13 +109,16 @@ type-of(Stuff, Type) :-
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-test(0, A) :- type-of([[list, [[int, 1], [string, 2]]]], A).
-test(1, A) :- type-of([[list, [[int, 1], [int, 2]]]], A).
-test(2, A) :- type-of([[list, [[int, 1]]]], A).
-test(3, A) :- type-of([[int, 1]], A).
-test(4, A) :- type-of([[float, 1.0]], A).
-test(5, A) :- type-of([[list, [[int, 1], [float, 2]]]], A).
-test(6, A) :- type-of([[application, [id, "print"], [int, 1]]], A).
-test(7, A) :- type-of([[application, [id, "print"], [float, 1]]], A).
-test(8, A) :- type-of([[application, [id, "identity"], [int, 1]]], A).
-test(9, A) :- type-of([[application, [id, "identity"], [float, 1]]], A).
+test( 0, A) :- type-of([[list, [[int, 1], [string, 2]]]], A).
+test( 1, A) :- type-of([[list, [[int, 1], [int, 2]]]], A).
+test( 2, A) :- type-of([[list, [[int, 1]]]], A).
+test( 3, A) :- type-of([[int, 1]], A).
+test( 4, A) :- type-of([[float, 1.0]], A).
+test( 5, A) :- type-of([[list, [[int, 1], [float, 2]]]], A).
+test( 6, A) :- type-of([[application, [id, "print"], [int, 1]]], A).
+test( 7, A) :- type-of([[application, [id, "print"], [float, 1]]], A).
+test( 8, A) :- type-of([[application, [id, "identity"], [int, 1]]], A).
+test( 9, A) :- type-of([[application, [id, "identity"], [float, 1]]], A).
+test(10, A) :- type-of([[new_var, "a", [int, 1], [[id, "a"]]]], A).
+test(11, A) :- type-of([ [application, [id, "map"], [id, "some-fun"], [list, [[int, 1]]]] ], A).
+
